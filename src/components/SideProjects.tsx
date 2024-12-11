eae487d389ef43995493e0f7e580cb2ac4004501
biaoli0 <lbit123@outlook.com>
2024-12-11 15:46:35 +1100

    Add followup

--- src/components/SideProjects.tsx
+++ src/components/SideProjects.tsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import { ProjectCard } from './ProjectCard';
-import { Project } from '../types/Project';
+import React from "react";
+import { ProjectCard } from "./ProjectCard";
+import { Project } from "../types/Project";
 
 const SideProjects = () => {
   const projects: Project[] = [
     {
-      title: 'API integration',
-      link: 'https://github.com/biaoli0/CryptoPriceSystem',
+      title: "API integration",
+      link: "https://github.com/biaoli0/CryptoPriceSystem",
       description:
-        'A serverless platform to query the current price of a specific cryptocurrency and receive the result from your email.',
-      image: 'images/side-projects/crypto.jpeg',
-      technologies: ['Node.js', 'TypeScript', 'Github Action', 'AWS'],
+        "A serverless platform to query the current price of a specific cryptocurrency and receive the result from your email.",
+      image: "images/side-projects/crypto.jpeg",
+      technologies: ["Node.js", "TypeScript", "Github Action", "AWS"],
+    },
+    {
+      title: "Followup RSS feeder",
+      link: "https://followup.biao-li.com",
+      description: `A RSS feed creator that allows you to create a RSS feed for your blog posts 
+         or any website you want to follow up.`,
+      image: "images/side-projects/followup.png",
+      technologies: [
+        "Node.js",
+        "TypeScript",
+        "Next.js",
+        "Tailwind",
+        "Nest.js",
+        "Anthropic API",
+      ],
     },
   ];
 
