1b04309e854b91fb1a69c77221ce997159f6cf44
Biao Li <lbit123@outlook.com>
2025-06-26 20:37:22 +1000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import './App.css';
-import Portfolio from './components/Protfolio';
+import React from "react";
+import "./App.css";
+import Portfolio from "./components/Portfolio";
 
 function App() {
   return (
